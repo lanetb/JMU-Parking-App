bdf287acd997682448821cd00f790f2cb144b210
Tommy Lane <72410142+lanetb@users.noreply.github.com>
2023-11-17 14:50:18 -0500

    Delete schema directory

--- schema/index.sql
+++
@@ -1,5 +0,0 @@
-CREATE INDEX ON meetings (course_nbr, term, days, start_t, end_t, enrolled, building_id);
-CREATE INDEX ON buildings (building_name);
-CREATE INDEX ON distances (minutes, distance, deck_id, building_id);
-CREATE INDEX ON decks (deck_name);
-CREATE INDEX ON observations(date_time, type, occ, deck_id);
